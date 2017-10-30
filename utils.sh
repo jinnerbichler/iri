ffbc4630249a5a89f87e847d029c0cbf2337f6de
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-10-30 19:09:30 +0100

    worked on proper testnet setup

--- utils.sh
+++ utils.sh
@@ -2,9 +2,22 @@
 
 set -e    # Exits immediately if a command exits with a non-zero status.sadfaksjdflakjsdf
 
-if [ "$1" == "docker-build" ]; then
+if [ "$1" == "full-deploy" ]; then
 
+    mvn clean package -DskipTests
     docker build -t jinnerbichler/iri_testnet .
+    docker push jinnerbichler/iri_testnet
+    scp docker-compose.yml utils.sh root@138.68.102.72:/srv/iri-testnet/
+    ssh root@138.68.102.72 "bash /srv/iri-testnet/utils.sh remote-testnet-start"
+
+elif [ "$1" == "docker-build" ]; then
+
+    mvn clean compile package -DskipTests
+    docker build -t jinnerbichler/iri_testnet .
+
+elif [ "$1" == "docker-run" ]; then
+
+    docker run -p "14265:14265" jinnerbichler/iri_testnet
 
 elif [ "$1" == "docker-push" ]; then
 
@@ -22,8 +35,9 @@ elif [ "$1" == "logs-testnet" ]; then
 elif [ "$1" == "remote-testnet-start" ]; then
 
     cd /srv/iri-testnet/
+    docker-compose down -v
     docker-compose pull
-    docker-compose up --build -d
+    docker-compose up -d --force-recreate
     docker-compose logs -f
 
 elif [ "$1" == "remote-testnet-logs" ]; then
